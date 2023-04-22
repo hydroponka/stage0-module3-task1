40eae20e226042604a815f628832fc51d291bab5
hydroponka <121126395+hydroponka@users.noreply.github.com>
2023-04-22 16:02:37 +0300

    Update PrintSentenceApp.java

--- src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
+++ src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class PrintSentenceApp {
     public static void main(String[] args) {
-        System.out.printf("java" + \n "is" + \n +"smth");
+        System.out.println("This is my first Java program");
     }
 }
